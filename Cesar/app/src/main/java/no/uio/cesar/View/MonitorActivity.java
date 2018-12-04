1300ec3c14baa813bd95fb07b601439f5b91ae91
Perelan <sinriat@gmail.com>
2018-12-04 16:31:53 +0100

    feat: add chronometer and handle monitor end event

--- Cesar/app/src/main/java/no/uio/cesar/View/MonitorActivity.java
+++ Cesar/app/src/main/java/no/uio/cesar/View/MonitorActivity.java
@@ -1,11 +1,17 @@
 package no.uio.cesar.View;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.view.Window;
+import android.widget.Chronometer;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import no.uio.cesar.R;
@@ -14,22 +20,18 @@ import no.uio.ripple.RippleEffect;
 public class MonitorActivity extends AppCompatActivity {
 
     private RippleEffect rp;
+    private Chronometer cm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> dialogSessionEnd());
+
+        cm = findViewById(R.id.monitor_time);
+        cm.start();
 
         rp = findViewById(R.id.ripple);
 
@@ -38,4 +40,27 @@ public class MonitorActivity extends AppCompatActivity {
         rp.pulse(rp.BATTERY);
     }
 
+    @Override
+    public void onBackPressed() {
+        dialogSessionEnd();
+    }
+
+    public void dialogSessionEnd() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you done with the monitor session?")
+                .setCancelable(false)
+                .setNegativeButton("NO!", null)
+                .setPositiveButton("Yes", (DialogInterface dialog, int which) -> {
+                    storeAndFinishSession();
+                }).create().show();
+    }
+
+    public void storeAndFinishSession() {
+        cm.stop();
+
+        // returns MS elapsed time
+        System.out.println(SystemClock.elapsedRealtime() - cm.getBase());
+
+        finish();
+    }
 }
